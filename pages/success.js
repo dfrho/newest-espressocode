179e7c627132baf365802302848aa12f0556c812
David Rhodes <david@espressocode.tech>
2023-03-21 14:08:24 -0500

    cleaner confetti

--- pages/success.js
+++ pages/success.js
@@ -1,23 +1,25 @@
 import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import Confetti from 'react-confetti'
+import useWindowSize from '../lib/use-window-size'
 
 const PaymentSuccessContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100vh;
-  background-color: #f2f2f2;
 `
 
 const PaymentSuccess = () => {
+  const { width, height } = useWindowSize()
   return (
-    <PaymentSuccessContainer>
-      <h1>Payment Successful!</h1>
-      <p>Thank you for your purchase.</p>
-      <Confetti />
-    </PaymentSuccessContainer>
+    <>
+      <PaymentSuccessContainer>
+        <h1>Payment Successful!</h1>
+        <p>Thank you for your purchase.</p>
+      </PaymentSuccessContainer>
+      <Confetti width={width} height={height} />
+    </>
   )
 }
 
